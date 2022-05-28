39e5588c6c907d2aaa071ac4a012ce68d5ac1f26
Lam-ss <ppoo44ttaa@gmail.com>
2022-05-29 00:53:53 +0300

    feat: fetch useEffect #6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from "./logo.svg";
+import React from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 import pizzas from "./assets/img/pizzas.json";
-
-console.log(pizzas);
+// https://629291fccd0c91932b73bb62.mockapi.io/items
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://629291fccd0c91932b73bb62.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -20,7 +32,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj, i) => (
+            {items.map((obj, i) => (
               <PizzaBlock {...obj} key={`${i}_pizzas`} />
             ))}
           </div>
