1621718dd8b347929b7196d9ea62331b7f59f914
Lam-ss <ppoo44ttaa@gmail.com>
2022-05-30 23:16:04 +0300

    feat: layout add sort, context #9

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = React.useState(0)
+function Categories({ value, onChangeCategory }) {
 
   const categories = [
     'Все',
@@ -12,16 +11,12 @@ function Categories() {
     'Закрытые',
   ]
 
-  const onClickCategory = (i) => {
-    setActiveIndex(i);
-  }
-
     return (
       <div className="categories">
         <ul>
-          {categories.map((value, i) => (
-              <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''} key={`${i}_${value}`}>
-                {value}
+          {categories.map((categoryName, i) => (
+              <li onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''} key={`${i}_${value}`}>
+                {categoryName}
               </li> 
             )
           )}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,22 @@ import Skeleton from "../components/PizzaBlock/Skeleton";
 const Home = () => {
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
+    const [categoryId, setCategoryId] = React.useState(0)
+    const [sortTipe, setSortTipe] = React.useState({
+      name: 'популярности',
+      sortTipe: 'rating',
+    })
   
     React.useEffect(() => {
-      fetch("https://629291fccd0c91932b73bb62.mockapi.io/items")
+      setIsLoading(true);
+      const order = sortTipe.sortProperty.includes('-') ? 'asc' : 'desc';
+      const sortBy = sortTipe.sortProperty.replace('-', '');
+
+      fetch(
+        `https://629291fccd0c91932b73bb62.mockapi.io/items?${
+          categoryId > 0 ? `category=${categoryId}` : ''
+          }&sortBy=${sortBy}&order=${order}`
+        )
         .then((res) => {
           return res.json();
         })
@@ -19,14 +32,14 @@ const Home = () => {
           setIsLoading(false);
         });
       window.scrollTo(0, 0);
-    }, []);
+    }, [categoryId, sortTipe]);
 
   return (
     <>
         <div className="container">
         <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortTipe} onChangeSort={(i) => setSortTipe(i)} />
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
